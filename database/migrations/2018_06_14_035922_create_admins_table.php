94c0a89516b995a478bd2270e1fb9ecee9ef1e8b
aung-zan <nightkiller10@gmail.com>
2018-06-14 11:27:08 +0630

    first push with migration

---
+++ database/migrations/2018_06_14_035922_create_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table ->bigIncrements('id');
+            $table ->string('username')            ->unique();
+            $table ->string('password');
+            $table ->rememberToken();
+            $table ->timestamps();
+            $table ->charset                     = 'utf8mb4';
+            $table ->collation                   = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
---
+++ database/migrations/2018_06_14_041857_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table ->bigIncrements('id');
+            $table ->bigInteger('companies_id') ->unsigned();
+            $table ->string('email')            ->unique();
+            $table ->string('password');
+            $table ->string('name')             ->nullable();
+            $table ->rememberToken();
+            $table ->timestamps();
+            $table ->charset                     = 'utf8mb4';
+            $table ->collation                   = 'utf8mb4_unicode_ci';
+        });
+
+        Schema::table('users', function($table) {
+            $table ->foreign('companies_id')    ->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
